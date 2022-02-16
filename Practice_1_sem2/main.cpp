77c7269472ecc515367b06c4e34b9de752f9a6cb
atmeed <leo.zemtsoff@gmail.com>
2022-02-16 16:24:35 +0300

    Create repos and add ready files

--- Practice_1_sem2/main.cpp
+++ Practice_1_sem2/main.cpp
@@ -1,14 +1,105 @@
-//
-//  main.cpp
-//  Practice_1_sem2
-//
-//  Created by Leonid Zemtsov on 16.02.2022.
+//  Created by Leonid Zemtsov on 15.02.2022.
 //
 
 #include <iostream>
+#include <string>
+#include <unistd.h> //Тк у меня Unix система)
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+
+
+/* Первое задание */
+void task1() {
+    using namespace std;
+    
+    int number1;
+    int number2;
+    string operation;
+    
+    //Ввод первого числа
+    cout << "Первое число: ";
+    cin >> number1;
+    cout << endl;
+    
+    //Ввод второго числа
+    cout << "Второе число: ";
+    cin >> number2;
+    cout << endl;
+    
+    //Ввод желаемой операции
+    cout << "Операция (+, -, /, *): ";
+    cin >> operation;
+    cout << endl;
+    
+    //switch не подходит(((
+    if (operation == "+") {
+        cout << number1 + number2 << endl;
+    } else if (operation == "-") {
+        cout << number1 - number2 << endl;
+    } else if (operation == "*") {
+        cout << number1 * number2 << endl;
+    } else if (operation == "/") {
+        cout << number1 / number2 << endl;
+    }
+    
+    
+    
+    
 }
+
+/* Второе задание */
+void task2(int n) {
+    while (n != 0) {
+        std::cout << n << std::endl;
+        sleep(1);
+        n--;
+    }
+    
+}
+
+/* Третье задание */
+void task3() {
+    using namespace std;
+    
+    string word;
+    int count;
+    
+    
+    cout << "Слово (В родительном падеже, ед.ч, пример: яблок): ";
+    cin >> word;
+    cout << endl;
+    
+    cout << "Колличество: ";
+    cin >> count;
+    cout << endl;
+    
+    switch (count) {
+        case 1:
+            cout << count << " " + word + "о";
+            cout << endl;
+            break;
+        case 2:
+        case 3:
+        case 4:
+            cout << count << " " + word + "a";
+            cout << endl;
+            break;
+        default:
+            cout << count << " " + word;
+            cout << endl;
+    }
+    
+    
+    
+}
+
+
+
+
+int main() {
+    
+    task1();
+    task2(rand());
+    task3();
+}
+
+
